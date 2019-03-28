f4f2255005a38ccd300d2b47c597404bd8463cb1
Ashot <macashot199514@gmail.com>
2019-03-28 15:24:14 +0400

    Initial Commit

---
+++ countrySearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  countrySearchApp
+//
+//  Created by Ashot on 3/28/19.
+//  Copyright © 2019 Ashot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
