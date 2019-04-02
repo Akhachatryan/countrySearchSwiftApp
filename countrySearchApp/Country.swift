6ffe02ae9837c87053f3bab126cd5e4c694eb582
Ashot <macashot199514@gmail.com>
2019-04-02 16:21:33 +0400

    first commit

---
+++ countrySearchApp/Country.swift
@@ -0,0 +1,9 @@
+//
+//  Country.swift
+//  countrySearchApp
+//
+//  Created by Ashot on 3/28/19.
+//  Copyright © 2019 Ashot. All rights reserved.
+//
+
+import Foundation
---
+++ countrySearchApp/Globals.swift
@@ -0,0 +1,9 @@
+//
+//  Globals.swift
+//  countrySearchApp
+//
+//  Created by Ashot on 3/28/19.
+//  Copyright © 2019 Ashot. All rights reserved.
+//
+
+import Foundation
